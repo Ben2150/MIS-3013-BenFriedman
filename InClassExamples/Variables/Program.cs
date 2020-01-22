9dec8f502483dcf855ee1718b7f5970c854d27c0
Bennett Friedman <Ben.friedman@ou.edu>
2020-01-22 12:55:34 -0600

    Created variables for firstName, lastName and favoriteNumber

--- InClassExamples/Variables/Program.cs
+++ InClassExamples/Variables/Program.cs
@@ -11,6 +11,27 @@ namespace Variables
     {
         static void Main(string[] args)
         {
+            //Create a string named firsName
+            string firstName;
+
+            //Assign a value to firstName
+            firstName = "Ben";
+
+
+            //Create an int named favoriteNumber and assign your favorite number to it
+            int favoriteNumber = 7;
+
+            double crazyNumber = 7.6767 * favoriteNumber;
+
+            string lastName = "Friedman";
+            string fullName = firstName + lastName;
+
+            Console.WriteLine(fullName + " has the favorite number of " + favoriteNumber
+                + " \nwhich equates to a crazy number of " + crazyNumber);
+
+            //MAKE IT DYNAMIC
+
+            Console.ReadKey();
         }
     }
 }
