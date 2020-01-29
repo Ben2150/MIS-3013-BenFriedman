f967d13bc87523ccf0ee12bc02736758063ffe5b
Bennett Friedman <Ben.friedman@ou.edu>
2020-01-29 13:15:04 -0600

    First part of HW1

---
+++ HW1Really/HW1Really/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1Really
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please choose item you are purchasing");
+            string itemname = Console.ReadLine();
+
+            Console.WriteLine("How many items are you purchasing?");
+            var input = ("");
+
+
+
+
+            Console.ReadKey();
+        }
+        
+    }
+}
