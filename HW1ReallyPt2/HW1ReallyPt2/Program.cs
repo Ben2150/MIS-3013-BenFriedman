de55e93e040e3e408c76ea7ecd5dc7837fb2ccff
Bennett Friedman <Ben.friedman@ou.edu>
2020-02-02 15:42:00 -0600

    Finsihed product Student Grade HW1

---
+++ HW1ReallyPt2/HW1ReallyPt2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1ReallyPt2
+{
+    class Program
+    {//Bennett Friedman
+        static void Main(string[] args)
+        {
+            string firstName;
+            string lastName;
+            int id;
+            double quizzes, participation, homeworks, midterm, finalexam;
+
+            Console.WriteLine("What is your first name?");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine("What is your student id?");
+            id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for homeworks?");
+            homeworks = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for participations?");
+            participation = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for quizzes?");
+            quizzes = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for the midterm?");
+            midterm = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for the final?");
+            finalexam = double.Parse(Console.ReadLine());
+
+            double finalgrade = (quizzes + participation + homeworks + midterm + finalexam) / 5;
+
+            Console.WriteLine(firstName + lastName + "(" + id + "), your final grade is" + finalgrade.ToString("F") + "%");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
