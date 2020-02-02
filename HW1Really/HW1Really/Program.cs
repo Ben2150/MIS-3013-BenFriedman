975ae7a30e0581f0d0f9de935127920591448fde
Bennett Friedman <Ben.friedman@ou.edu>
2020-02-02 15:04:58 -0600

    Finished product of Sale Total HW 1

--- HW1Really/HW1Really/Program.cs
+++ HW1Really/HW1Really/Program.cs
@@ -8,16 +8,30 @@ namespace HW1Really
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            const double SalesTax = .085;//8.5%
+            string itemname;
+            int quant;
+            double pri;
+
             Console.WriteLine("Please choose item you are purchasing");
-            string itemname = Console.ReadLine();
+            itemname = Console.ReadLine();
 
-            Console.WriteLine("How many items are you purchasing?");
-            var input = ("");
+            Console.WriteLine("How many " + itemname + " do you want to buy?");
+            quant = int.Parse(Console.ReadLine());
 
+            Console.WriteLine("What is the price for each " + itemname + "?");
+            pri = double.Parse(Console.ReadLine());
 
+            double subtotal = pri * quant;
+            double salestax = SalesTax * subtotal;
+            double salestotal = subtotal + salestax;
 
+            Console.WriteLine("Your subtotal for your bill is " + subtotal.ToString("C") + "\nYour sales tax for your bill is" + salestax.ToString("C") + "\nYour total for your bill is" + salestotal.ToString("C"));
+
+            Console.WriteLine("Press any key to continue...");
 
             Console.ReadKey();
         }
